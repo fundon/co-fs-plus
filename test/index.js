c952a61d75d0b71962b55a70fe1e9533c87bf62d
fundon <cfddream@gmail.com>
2014-03-11 06:24:19 +0800

    test `fs.mkdirp`

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 var co = require('co');
 var fs = require('..');
+var path = require('path');
 
 describe('.walk()', function () {
   it('should work', function (done) {
@@ -7,6 +8,15 @@ describe('.walk()', function () {
       var files = yield fs.walk('test/fixtures')
       files.should.have.length(1);
       files[0].should.equal('test/fixtures/msg.txt');
+    })(done);
+  });
+});
+
+describe('.mkdirp()', function () {
+  it('should work', function (done) {
+    co(function *() {
+      var res = yield fs.mkdirp('test/a/b/c/d/e/f/g')
+      res.path.should.equal(path.resolve('test/a/b/c/d/e/f/g'));
     })(done);
   });
 });
